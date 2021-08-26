9106d583d8b9b129d9b6fc0b29e0587ad0e6f70b
goxzero1 <goxzero@skuniv.ac.kr>
2021-08-26 17:06:33 +0900

    commit

--- app/src/src/main/java/com/example/momogae/MainActivity/Main.java
+++ app/src/src/main/java/com/example/momogae/MainActivity/Main.java
@@ -8,8 +8,8 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.momogae.Board.BoardActivity;
-import com.example.momogae.Map.ClassificationActivity;
 import com.example.momogae.Diary.DiaryActivity;
+import com.example.momogae.Map.ClassificationActivity;
 import com.example.momogae.R;
 
 public class Main extends AppCompatActivity {
